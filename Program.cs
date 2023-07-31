50c0a8053a1461c345e9d4e770803c68790f9d06
xandelm <alexandrej.lima.mendes@gmail.com>
2023-07-31 19:57:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿var input1 = Console.ReadLine();
+var input2 = Console.ReadLine();
+
+int num1 = System.Convert.ToInt32(input1);
+int num2 = System.Convert.ToInt32(input2);
+
+Func<int, int,(int, int, int,int,int)> calc = (x, y) => (x+y,x-y,x*y,x/y,x%y);
+Console.WriteLine(calc(num1,num2));
